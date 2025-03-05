57ee02a6850b489bbd9211379b35a0021ae57a17
catheriz <53827903+catheriz@users.noreply.github.com>
2025-03-04 19:03:15 -0600

    Update Contrusct_Co_expression.R

--- Other Resource/Contrusct_Co_expression.R
+++ Other Resource/Contrusct_Co_expression.R
@@ -1,80 +1,76 @@
-library(reshape)
+# Load necessary libraries
+library(reshape2)
 library(igraph)
-library("biomaRt")
-data.tissue <-read.csv('RNA_consensus_tissue_gene_data/rna_tissue_consensus.tsv',head=T,sep='\t')
-data.celline = read.csv('RNA_HPA_immune_cell_sample_gene_data/rna_immune_cell_sample.tsv',head=T,sep='\t')
-data.celline = data.celline[c(4,5,3,8)]
-colnames(data.tissue) = c('Ensemble_ID', 'Gene.name', 'Sample', 'Value')
-colnames(data.celline) = c('Ensemble_ID', 'Gene.name', 'Sample', 'Value')
-data.tissue = data.tissue[which((data.tissue$Sample=='lung')|(data.tissue$Sample=='kidney')),]
-data.celline=data.celline[which((data.celline$Sample=='NK-cell')|(data.celline$Sample=='T-reg')|(data.celline$Sample=='naive B-cell')|(data.celline$Sample=='naive CD8 T-cell')|(data.celline$Sample=='memory CD8 T-cell')|(data.celline$Sample=='naive CD4 T-cell')|(data.celline$Sample=='memory CD4 T-cell')),]
-data_Coexpr <- rbind(data.tissue,data.celline)
+library(biomaRt)
 
-check.HGNC.symbols <- function(GeneNames){
-  
-  ensembl <- useMart("ensembl",dataset="hsapiens_gene_ensembl")
-  
-  ensemble_HGNC <- getBM(attributes=c('hgnc_symbol'), 
-                         filters='hgnc_symbol', values=GeneNames, mart=ensembl)
-  
+# Read tissue and immune cell expression data
+data.tissue <- read.csv('RNA_consensus_tissue_gene_data/rna_tissue_consensus.tsv', head = TRUE, sep = '\t')
+data.celline <- read.csv('RNA_HPA_immune_cell_sample_gene_data/rna_immune_cell_sample.tsv', head = TRUE, sep = '\t')
+
+# Select relevant columns
+data.celline <- data.celline[c(4,5,3,8)]
+
+# Standardize column names
+colnames(data.tissue) <- c('Ensemble_ID', 'Gene.name', 'Sample', 'Value')
+colnames(data.celline) <- c('Ensemble_ID', 'Gene.name', 'Sample', 'Value')
+# Combine datasets
+data_Coexpr <- rbind(data.tissue, data.celline)
+
+# Function to check and validate HGNC symbols
+check.HGNC.symbols <- function(GeneNames) {
+  ensembl <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+  ensemble_HGNC <- getBM(attributes = c('hgnc_symbol'), 
+                         filters = 'hgnc_symbol', 
+                         values = GeneNames, 
+                         mart = ensembl)
   return(ensemble_HGNC$hgnc_symbol)
 }
+
+# Validate HGNC gene names
 ensemble_HGNC <- check.HGNC.symbols(unique(data_Coexpr$Gene.name))
-data3_Coexpr <- data_Coexpr[which(data_Coexpr$Gene.name %in% ensemble_HGNC),]
+data3_Coexpr <- data_Coexpr[data_Coexpr$Gene.name %in% ensemble_HGNC, ]
 
-matrix_Coexpr<-cast(data3_Coexpr, Gene.name ~ Sample,mean,value="Value") 
-rownames(matrix_Coexpr)<-matrix_Coexpr[,1]
-matrix_Coexpr[,1]<-NULL
-#remove rows with NA mean
-c = which(is.na(matrix_Coexpr))
-matrix_Coexpr = matrix_Coexpr[which(!rownames(matrix_Coexpr)%in% c),]
+# Create expression matrix
+matrix_Coexpr <- dcast(data3_Coexpr, Gene.name ~ Sample, mean, value.var = "Value")
 
-# Identify rows (or columns) with zero standard deviation
-sd_values <- apply(matrix_Coexpr, 1, sd)  # For rows
-zero_sd_rows <- which(sd_values == 0)
+# Set row names
+rownames(matrix_Coexpr) <- matrix_Coexpr[, 1]
+matrix_Coexpr <- matrix_Coexpr[, -1]
 
-sd_values_col <- apply(matrix_Coexpr, 2, sd)  # For columns
-zero_sd_cols <- which(sd_values_col == 0)
+# Remove rows with missing values
+matrix_Coexpr <- na.omit(matrix_Coexpr)
 
-# Display problematic rows and columns
-print("Rows with zero standard deviation:")
-print(zero_sd_rows)
+# Identify rows and columns with zero standard deviation
+zero_sd_rows <- apply(matrix_Coexpr, 1, sd) == 0
+zero_sd_cols <- apply(matrix_Coexpr, 2, sd) == 0
 
-print("Columns with zero standard deviation:")
-print(zero_sd_cols)
+# Remove zero-variance rows and columns
+matrix_Coexpr <- matrix_Coexpr[!zero_sd_rows, ]
+matrix_Coexpr <- matrix_Coexpr[, !zero_sd_cols]
 
-# Remove rows with zero standard deviation
-if (length(zero_sd_rows) > 0) {
-  matrix_Coexpr <- matrix_Coexpr[-zero_sd_rows, ]
-}
-
-# Remove columns with zero standard deviation
-if (length(zero_sd_cols) > 0) {
-  matrix_Coexpr <- matrix_Coexpr[, -zero_sd_cols]
-}
-
-# Transpose if needed and compute the correlation matrix
+# Compute Spearman correlation matrix
 CR <- cor(t(matrix_Coexpr), method = "spearman")
 
-diag(CR) = 0
-inds <- which(abs(CR[1:nrow(CR),]) >= 0.75, arr.ind=TRUE)
-inds = as.data.frame(inds)
-cor_value = c()
-for (i in 1:dim(inds)[1]){
-  cor_value[i] = CR[inds$row[i],inds$col[i]]
-}
-inds$weight = abs(cor_value)
-row_name =c()
-col_name = c()
+# Set diagonal to zero (self-correlations are not meaningful)
+diag(CR) <- 0
 
-for (i in 1:dim(inds)[1]){
-  row_name[i] = rownames(CR)[inds$row[i]]
-  col_name[i] = colnames(CR)[inds$col[i]]
-}
-inds$row_name = row_name
-inds$col_name = col_name
+# Extract significant correlations (absolute value ≥ 0.75)
+inds <- which(abs(CR) >= 0.75, arr.ind = TRUE)
+inds <- as.data.frame(inds)
 
-inds$network_weight=(inds$weight/min(inds$weight))
-co_express_network_edge_list = inds[c(5,6,4)]
-write.csv(co_express_network_edge_list,'Coexpression_AAV_edgelist_corr0.75_with_weight.csv',quote=F,row.names=F,col.names=T)
+# Extract correlation values
+inds$weight <- abs(CR[inds$row, inds$col])
 
+# Retrieve gene names
+inds$row_name <- rownames(CR)[inds$row]
+inds$col_name <- colnames(CR)[inds$col]
+
+# Normalize network weights
+inds$network_weight <- inds$weight / min(inds$weight)
+
+# Create edge list for network analysis
+co_express_network_edge_list <- inds[, c("row_name", "col_name", "network_weight")]
+
+# Save edge list to CSV
+write.csv(co_express_network_edge_list, 'Coexpression_network_edgelist_corr0.75_with_weight.csv', 
+          quote = FALSE, row.names = FALSE)
